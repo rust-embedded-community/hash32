8940d964511622c977bbffbc7e323fa0450fb38c
Alex Martens <alex@thinglab.org>
2025-03-25 17:56:07 -0700

    Add test vectors for FnvHasher

--- src/fnv.rs
+++ src/fnv.rs
@@ -4,6 +4,10 @@ const BASIS: u32 = 0x811c9dc5;
 const PRIME: u32 = 0x1000193;
 
 /// 32-bit Fowler-Noll-Vo hasher
+///
+/// Specifically this implements the [FNV-1a hash].
+///
+/// [FNV-1a hash]: https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function#FNV-1a_hash
 pub struct Hasher {
     state: u32,
 }
